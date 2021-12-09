de2b7e7f1c786c476402236571887d48f7e3d39f
emre15900 <79264023+emre15900@users.noreply.github.com>
2021-12-09 14:50:44 +0300

    fixed swiper

--- javascript/home.js
+++ javascript/home.js
@@ -1,28 +1,25 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
-    spaceBetween: 50,
-    loop: false,
-    scrollbar: {
-        el: ".swiper-scrollbar",
-        hide: true,
-      },
+    spaceBetween: 20,
+    loop: true,
     breakpoints: {
       "@0.00": {
         slidesPerView: 1,
-        spaceBetween: 10,
       },
       "@0.75": {
         slidesPerView: 2,
-        spaceBetween: 20,
       },
       "@1.00": {
         slidesPerView: 3,
-        spaceBetween: 40,
       },
       "@1.50": {
         slidesPerView: 4,
-        spaceBetween: 50,
       },
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
 
   });
+
