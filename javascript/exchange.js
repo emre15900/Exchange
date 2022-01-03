3b6fa2d5e2c015b0a7c21604ffa54918a49e2190
Emre Sayın <79264023+emre15900@users.noreply.github.com>
2022-01-04 02:29:26 +0300

    Revert "Merge branch 'main' of https://github.com/emre15900/Exchange"
    This reverts commit 7f13f3e1d4a0e13013c03f294f150993c1bb4735, reversing
    changes made to ce19094e07d6e13f2d9ef25005002db6130bb85d.

--- javascript/exchange.js
+++ javascript/exchange.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
+  //Market Buy Sell Active
   $(".button").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
+  });
+
+  //Search Filter
+  $(".search-button").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".coin-item").filter(function () {
+      $(this).toggle($(this).text().toLocaleLowerCase().indexOf(value) > -1);
+    });
   });
 });
